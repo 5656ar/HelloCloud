598a12096d6d17974e8c041ee031493f1abef7ec
5656ar <nuat123456@gmail.com>
2022-09-29 21:31:12 +0700

    up

--- guestbook7.py
+++ guestbook7.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Date
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:BQEhbz67958@node37003-bunnapon.proen.app.ruk-com.cloud:5432/testdb'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:BQEhbz67958@10.104.9.222:5432/testdb'
 app.config['SQLALCHEMY_TRACK_MPDIFICATION'] = False
 
 db = SQLAlchemy(app)
@@ -33,4 +33,5 @@ def process():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host= '0.0.0.0',port= 80,debug=True)
+    
