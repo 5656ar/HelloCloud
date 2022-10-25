f10747ffa2faecc235c3140e3de8fd36e5fe4ea1
5656ar <nuat123456@gmail.com>
2022-10-25 21:05:40 +0700

    p

--- guestbook8.py
+++ guestbook8.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, ForeignKey, Integer, String, Date, CHAR, VARCHAR, ForeignKey
+from sqlalchemy import Column, Integer, String, Date
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:BQEhbz67958@10.104.9.222:5432/testdb'
@@ -9,12 +9,21 @@ app.config['SQLALCHEMY_TRACK_MPDIFICATION'] = False
 db = SQLAlchemy(app)
 
 class Comments(db.Model):
-    __tablename__ = 'Aomtedz'
-    student_id = Column(CHAR(13),ForeignKey('Students.student_id'), nullable=False)
-    subject_id = Column(VARCHAR(15), ForeignKey('Subject.subject_id'), nullable=False)
-    year = Column(CHAR(4), nullable=False)
-    semester = Column(CHAR(1), nullable=False)
-    grade = Column(CHAR(2), primary_key=True)
+    __tablename__ = 'Aomme'
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    comment = Column(String)
+
+@app.route('/')
+def index():
+    result = Comments.query.all()
+    return render_template('index7.html', result=result)
+
+@app.route('/sign')
+def sign():
+    return render_template('sign7.html')
+
+
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',port= 80,debug=True)
