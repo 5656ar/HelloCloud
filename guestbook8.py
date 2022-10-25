1d6e1db7e681cf80cc9bff7f2c7407585bb9f65d
5656ar <nuat123456@gmail.com>
2022-10-25 22:01:23 +0700

    up

--- guestbook8.py
+++ guestbook8.py
@@ -28,11 +28,7 @@ def sign():
 
 @app.route('/process', methods=['POST'])
 def process():
-    name = request.form['name']
-    comment = request.form['comment']
-    signature = Comments(name=name, comment=comment)
-    db.session.add(signature)
-    db.session.commit()
+
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
