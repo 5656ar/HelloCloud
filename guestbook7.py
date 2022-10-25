3da17c6a10e59730006fb72f78ad0b34ded4634e
5656ar <nuat123456@gmail.com>
2022-10-25 21:57:38 +0700

    up

--- guestbook7.py
+++ guestbook7.py
@@ -19,18 +19,7 @@ def index():
     result = Comments.query.all()
     return render_template('index7.html', result=result)
 
-@app.route('/sign')
-def sign():
-    return render_template('sign7.html')
 
-@app.route('/process', methods=['POST'])
-def process():
-    name = request.form['name']
-    comment = request.form['comment']
-    signature = Comments(name=name, comment=comment)
-    db.session.add(signature)
-    db.session.commit()
-    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',port= 80,debug=True)
