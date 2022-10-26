37c19d64c5cba6855284a169d0371546b290d7e1
5656ar <nuat123456@gmail.com>
2022-10-27 00:02:34 +0700

    u

--- guestbook8.py
+++ guestbook8.py
@@ -21,7 +21,7 @@ class Comments(db.Model):
 @app.route('/')
 def index():
     result = Comments.query.all()
-    return render_template('index7.html', result=result)
+    return render_template('index8.html', result=result)
 
 @app.route('/sign')
 def sign():
