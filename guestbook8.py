cad5b3993aa4f9e95244098c5c9b2bcc26a4586a
5656ar <nuat123456@gmail.com>
2022-10-26 16:10:08 +0700

    u

--- guestbook8.py
+++ guestbook8.py
@@ -1,7 +1,6 @@
-from test_ex_1 import Registration_table
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import Column, Integer, String, Date, CHAR, VARCHAR
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:BQEhbz67958@10.104.9.222:5432/testdb'
@@ -9,11 +8,18 @@ app.config['SQLALCHEMY_TRACK_MPDIFICATION'] = False
 
 db = SQLAlchemy(app)
 
-
+class Comments(db.Model):
+    __tablename__ = 'Registration'
+    id = Column(Integer, primary_key=True)
+    student_id = Column(CHAR(13), nullable=False)
+    subject_id = Column(VARCHAR(15), nullable=False)
+    year = Column(CHAR(4), nullable=False)
+    semester = Column(CHAR(1), nullable=False)
+    grade = Column(CHAR(2))
 
 @app.route('/')
 def index():
-    result = Registration_table.query.all()
+    result = Comments.query.all()
     return render_template('index8.html', result=result)
 
 @app.route('/sign')
@@ -22,11 +28,7 @@ def sign():
 
 @app.route('/process', methods=['POST'])
 def process():
-    name = request.form['name']
-    comment = request.form['comment']
-    signature = Comments(name=name, comment=comment)
-    db.session.add(signature)
-    db.session.commit()
+
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
