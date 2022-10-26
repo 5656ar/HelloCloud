1850411a1555bf3d195ea909e0ce2a7824410790
5656ar <nuat123456@gmail.com>
2022-10-27 00:04:45 +0700

    u

--- guestbook8.py
+++ guestbook8.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Date, CHAR, VARCHAR
+from test_ex_1 import Registration_table
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:MDDnfo15110@10.104.7.84:5432/testdb'
