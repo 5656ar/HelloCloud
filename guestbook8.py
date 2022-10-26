928d49eda49d52173ffc81fe8ce2912015669c98
5656ar <nuat123456@gmail.com>
2022-10-26 16:12:33 +0700

    u

--- guestbook8.py
+++ guestbook8.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Date, CHAR, VARCHAR
+from test_ex_1 import Registration_table
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:BQEhbz67958@10.104.9.222:5432/testdb'
@@ -8,18 +8,11 @@ app.config['SQLALCHEMY_TRACK_MPDIFICATION'] = False
 
 db = SQLAlchemy(app)
 
-class Comments(db.Model):
-    __tablename__ = 'Registration'
-    id = Column(Integer, primary_key=True)
-    student_id = Column(CHAR(13), nullable=False)
-    subject_id = Column(VARCHAR(15), nullable=False)
-    year = Column(CHAR(4), nullable=False)
-    semester = Column(CHAR(1), nullable=False)
-    grade = Column(CHAR(2))
+
 
 @app.route('/')
 def index():
-    result = Comments.query.all()
+    result = Registration_table.query.all()
     return render_template('index8.html', result=result)
 
 @app.route('/sign')
