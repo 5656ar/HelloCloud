0817fe811e186208b5b3ebbc32fcef277e3b3e9c
5656ar <nuat123456@gmail.com>
2022-10-27 01:50:02 +0700

    u

--- guestbook8.py
+++ guestbook8.py
@@ -17,7 +17,12 @@ class Student(db2.Model):
     l_name = Column(VARCHAR(30), nullable=False)
     e_mail = Column(VARCHAR(50),nullable=False)
 
-
+class Student(db.Model):
+    __tablename__ = "Subject"
+    subject_id = Column(VARCHAR(15), primary_key=True, nullable=False)
+    subject_name = Column(VARCHAR(50), nullable=False)
+    cradit = Column(Integer, nullable=False)
+    teacher_id = Column(CHAR(3))
 
   
 @app.route('/')
