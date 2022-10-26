a934539245beb7e6e6f3b914c32c9026705aa544
5656ar <nuat123456@gmail.com>
2022-10-27 01:43:07 +0700

    u

--- guestbook8.py
+++ guestbook8.py
@@ -27,8 +27,8 @@ class Subjects(db.Model):
   
 @app.route('/')
 def index():
-    result1 = Subjects.query.all()
-    result = Student.query.all()
+    result = Subjects.query.all()
+    result1 = Student.query.all()
     # result += result1
     return render_template('index8.html', result1=result1)
 
