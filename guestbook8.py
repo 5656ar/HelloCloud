0434d902c7f4e758e0b7159444a1af669e53ece0
5656ar <nuat123456@gmail.com>
2022-10-27 01:26:31 +0700

    u

--- guestbook8.py
+++ guestbook8.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Date, CHAR, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:MDDnfo15110@10.104.7.84:5432/testdb'
 app.config['SQLALCHEMY_TRACK_MPDIFICATION'] = False
 
-db = SQLAlchemy(app)
-
-class Student(db.Model):
+Base = declarative_base(app)
+class Student(Base.Model):
     __tablename__ = 'Students'
     student_id = Column(CHAR(13),primary_key=True, nullable=False)
     f_name = Column(VARCHAR(30), nullable=False)
