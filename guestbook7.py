557f6dbeb0ac4cc7584cdb6a47dbf0a611617695
5656ar <nuat123456@gmail.com>
2022-10-26 23:58:08 +0700

    u

--- guestbook7.py
+++ guestbook7.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Date, CHAR, VARCHAR
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:MDDnfo15110@10.104.9.222:5432/testdb'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:MDDnfo15110@10.104.9.155:5432/testdb'
 app.config['SQLALCHEMY_TRACK_MPDIFICATION'] = False
 
 db = SQLAlchemy(app)
