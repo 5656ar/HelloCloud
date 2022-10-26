cdaed645ef9a58ba538194de971ae77226c75a12
5656ar <nuat123456@gmail.com>
2022-10-26 16:17:13 +0700

    u

--- guestbook8.py
+++ guestbook8.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Date, CHAR, VARCHAR
-from test_ex_1 import Registration_table
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:BQEhbz67958@10.104.9.222:5432/testdb'
@@ -20,7 +20,7 @@ class Comments(db.Model):
 
 @app.route('/')
 def index():
-    result1 = Registration_table.query.all()
+
     result = Comments.query.all()
     return render_template('index8.html', result=result)
 
