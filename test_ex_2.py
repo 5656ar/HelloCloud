276d2356f00fcfee18e08bfa38bc18eeb2ab81c3
5656ar <nuat123456@gmail.com>
2022-10-27 11:11:43 +0700

    u

--- test_ex_2.py
+++ test_ex_2.py
@@ -1,31 +1,3 @@
-from sqlalchemy import CHAR, VARCHAR, Column, Integer, ForeignKey
-import os
-from flask import Flask, render_template, request, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
-
-from sqlalchemy.sql import func
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class Registration_table(db.Model):
-    __tablename__ = 'Registration'
-    id = db.Column(Integer, primary_key=True)
-    student_id = db.Column(CHAR(13), nullable=False)
-    subject_id = db.Column(VARCHAR(15), nullable=False)
-    year = db.Column(CHAR(4), nullable=False)
-    semester = db.Column(CHAR(1), nullable=False)
-    grade = db.Column(CHAR(2))
-
-    def __repr__(self):
-        return '<User(student_id = {}, subject_id = {}, year = {}, semester ={}, grade={})>'.format(self.student_id, \
-            self.subject_id, self.year , self.semester, self.grade)
-    
-
-
+result = session.query(Student.student_id,Student.f_name,Registration.subject_id,Subjects.subject_name,Registration.grade,Teachers.f_tname,Teachers.l_tname)\
+        .outerjoin(Registration,Student.student_id == Registration.student_id)\
+        .outerjoin(Subjects,Registration.subject_id == Subjects.subject_id).join(Teachers,Subjects.teacher_id == Teachers.teacher_id).all()
