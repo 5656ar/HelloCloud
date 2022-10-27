1c863fec3f9f02cf81bc2e867e38b831e4e664e3
5656ar <nuat123456@gmaio.com>
2022-10-27 14:41:16 +0700

    u

--- guestbook7.py
+++ guestbook7.py
@@ -1,37 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Date, CHAR, VARCHAR
+from test_ex_1 import Registration_table, Students_table, Teachers_table, Subject_table, session
+
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:MDDnfo15110@10.104.7.84:5432/testdb'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:MDDnfo15110@10.104.7.84:11234/testdb'
 app.config['SQLALCHEMY_TRACK_MPDIFICATION'] = False
 
-db = SQLAlchemy(app)
-
-class Comments(db.Model):
-    __tablename__ = 'comments'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    comment = Column(String)
 
 @app.route('/')
 def index():
-    result = Comments.query.all()
-    return render_template('index7.html', result=result)
+    result = session.query(Students_table.student_id,Students_table.f_name,Students_table.l_name,Registration_table.subject_id,Subject_table.subject_name,Registration_table.grade,Teachers_table.f_tname,Teachers_table.l_tname).outerjoin(Registration_table,Students_table.student_id == Registration_table.student_id).outerjoin(Subject_table,Registration_table.subject_id == Subject_table.subject_id).join(Teachers_table,Subject_table.teacher_id == Teachers_table.teacher_id).all()
+    return render_template('index8.html', result=result)
 
 @app.route('/sign')
 def sign():
     return render_template('sign7.html')
 
-@app.route('/process', methods=['POST'])
-def process():
-    name = request.form['name']
-    comment = request.form['comment']
-    signature = Comments(name=name, comment=comment)
-    db.session.add(signature)
-    db.session.commit()
-    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',port= 80,debug=True)
--- test_ex_2.py
+++ test_ex_2.py
@@ -1,29 +1,38 @@
-<body>
-    <div class="container theme-showcase" role="main">
-        <div class="page-header">
-            <tr>
-                <th>student-id</th>
-                <th>student first name</th>
-                <th>student last name</th>
-                <th>subject id</th>
-                <th>subject name</th>
-                <th>teacher first name</th>
-                <th>teacher last name</th>
-            </tr>
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Date, CHAR, VARCHAR
 
-            {% for r in result %}
-            <tr>
-                <th>{{ r.student_id }}</th>
-                <th>{{ r.f_name }}</th>
-                <th>{{ r.l_name }}</th>
-                <th>{{ r.subject_id }}</th>
-                <th>{{ r.grade}}</th>
-                <th>{{ r.f_tname}}</th>
-                <th>{{ r.l_tname}}</th>
-            </tr>
-        </div>
-        {% endfor %}
-    </div> <!--/container -->
-</body>
 
- result = session.query(Students_table.student_id,Students_table.f_name,Students_table.l_name,Registration_table.subject_id,Subject_table.subject_name,Registration_table.grade,Teachers_table.f_tname,Teachers_table.l_tname).outerjoin(Registration_table,Students_table.student_id == Registration_table.student_id).outerjoin(Subject_table,Registration_table.subject_id == Subject_table.subject_id).join(Teachers_table,Subject_table.teacher_id == Teachers_table.teacher_id).all()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:MDDnfo15110@10.104.7.84:5432/testdb'
+app.config['SQLALCHEMY_TRACK_MPDIFICATION'] = False
+
+db = SQLAlchemy(app)
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    comment = Column(String)
+
+@app.route('/')
+def index():
+    result = Comments.query.all()
+    return render_template('index7.html', result=result)
+
+@app.route('/sign')
+def sign():
+    return render_template('sign7.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+    signature = Comments(name=name, comment=comment)
+    db.session.add(signature)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',port= 80,debug=True)
+    
